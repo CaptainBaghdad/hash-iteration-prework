156a5298fc4b58d380f7b8ab44dca08db038c405
Jason Pitts <juniorjr083@gmail.com>
2018-06-10 23:31:31 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,6 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  na = passengers.select{|k,v|  k == suite_a: && v.start_with?("A")}
+  na = passengers.select{|k,v|  k == suite_a:}
   na.join
 end
